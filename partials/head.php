26e7e8caa008db95bf81ff2621160db6e1da1e94
mikahikaai <mikahikaai@gmail.com>
2022-04-11 12:42:31 +0800

    add fa6 offline

--- partials/head.php
+++ partials/head.php
@@ -8,10 +8,11 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
   <!-- <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css"> -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="./plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
 
+  <script src="./plugins/fontawesome-free/js/all.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.2/dist/umd/popper.min.js" crossorigin="anonymous"></script>
   <script src="./plugins/tempusdominus-bootstrap-4/js/tempus-dominus.min.js"></script>
   <link rel="stylesheet" href="./plugins/tempusdominus-bootstrap-4/css/tempus-dominus.min.css">
