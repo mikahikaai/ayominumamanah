de7342c36ac62ba2c828eed2ab46b09b669b6512
mikahikaai <mikahikaai@gmail.com>
2022-04-14 17:28:06 +0800

    update db

--- db/ayominumamanah.sql
+++ db/ayominumamanah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 14, 2022 at 02:38 AM
+-- Generation Time: Apr 14, 2022 at 09:27 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.4.19
 
