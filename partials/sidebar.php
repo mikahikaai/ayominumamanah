23d149d36283ffbbec25127564f52fa173427d00
mikahikaai <mikahikaai@gmail.com>
2022-06-20 14:35:38 +0800

    fix some minor bugs on user photo and name

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -18,27 +18,17 @@
     <img src="../dist/img/AdminLTELogo.png" alt="AMDK Amanah" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">AMDK Amanah</span>
   </a>
+  <a href="#" class="brand-link">
+    <img src="../dist/img/<?= file_exists("../dist/img/" . ($_SESSION['foto'] == NULL ? 'null' : $_SESSION['foto'])) ? $_SESSION['foto'] : 'avatarm.png'; ?>" class="brand-image img-circle elevation-3" alt="User Image">
+    <span class="brand-text align-text-top" style="font-size: 16px;"><?= $_SESSION['nama']; ?></span>
+  </a>
 
   <!-- Sidebar -->
   <div class="sidebar">
     <!-- Sidebar user panel (optional) -->
-    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-      <div class="image">
-        <?php
-        // var_dump(file_exists("../dist/img/" . "NULL"));
-        // die();
-        // var_dump($_SESSION['foto']);
-        // die();
-        ?>
-        <img src="../dist/img/<?= file_exists("../dist/img/" . ($_SESSION['foto'] == NULL ? 'null' : $_SESSION['foto'])) ? $_SESSION['foto'] : 'avatarm.png'; ?>" class="img-circle elevation-2" alt="User Image">
-      </div>
-      <div class="info">
-        <a href="#" target="_Blank" class="d-block"><?= $_SESSION['nama']; ?></a>
-      </div>
-    </div>
 
     <!-- SidebarSearch Form -->
-    <div class="form-inline">
+    <div class="form-inline mt-3">
       <div class="input-group" data-widget="sidebar-search">
         <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
         <div class="input-group-append">
@@ -94,8 +84,8 @@
               </li>
             </ul>
           </li>
-        <?php } else if ($_SESSION['jabatan'] == 'MGRDISTRIBUSI'){
-          ?>
+        <?php } else if ($_SESSION['jabatan'] == 'MGRDISTRIBUSI') {
+        ?>
           <li class="nav-item" id="pengajuanupah">
             <a href="#" class="nav-link" id="link_pengajuanupah">
               <i class="nav-icon fas fa-money-bill"></i>
@@ -121,79 +111,79 @@
           </li>
         <?php }; ?>
         <li class="nav-item" id="rekapitulasi">
-            <a href="#" class="nav-link" id="link_rekapitulasi">
+          <a href="#" class="nav-link" id="link_rekapitulasi">
             <i class="nav-icon fas fa-paperclip"></i>
-              <p>
-                Rekapitulasi
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
+            <p>
+              Rekapitulasi
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="?page=rangerekappengajuanupah" class="nav-link" id="rekappengajuanupah">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Rekap Pengajuan Upah</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="?page=rangerekapinsentif" class="nav-link" id="rekappengajuaninsentif">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Rekap Pengajuan Insentif</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="?page=rangerekapupah" class="nav-link" id="rekapupah">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Rekap Upah Borongan</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="?page=rangerekapinsentif" class="nav-link" id="rekapinsentif">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Rekap Insentif Borongan</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item" id='master_data'>
+          <a href="#" class="nav-link" id='link_master_data'>
+            <i class="fas fa-th nav-icon"></i>
+            <p>
+              Master Data
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <?php
+            if ($_SESSION['jabatan'] == "ADMINKEU") {
+            ?>
               <li class="nav-item">
-                <a href="?page=rangerekappengajuanupah" class="nav-link" id="rekappengajuanupah">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Rekap Pengajuan Upah</p>
+                <a href="?page=armadaread" class="nav-link" id='armada'><i class="far fa-circle nav-icon"></i>
+                  <p>Armada</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="?page=rangerekapinsentif" class="nav-link" id="rekappengajuaninsentif">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Rekap Pengajuan Insentif</p>
+                <a href="?page=karyawanread" class="nav-link" id='karyawan'><i class="far fa-circle nav-icon"></i>
+                  <p>Karyawan</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="?page=rangerekapupah" class="nav-link" id="rekapupah">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Rekap Upah Borongan</p>
+                <a href="?page=distributorread" class="nav-link" id="distributor"><i class="far fa-circle nav-icon"></i>
+                  <p>Distributor</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="?page=rangerekapinsentif" class="nav-link" id="rekapinsentif">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Rekap Insentif Borongan</p>
+                <a href="?page=distribusiread" class="nav-link" id="distribusi"><i class="far fa-circle nav-icon"></i>
+                  <p>Distribusi</p>
                 </a>
               </li>
-            </ul>
-          </li>
-          <li class="nav-item" id='master_data'>
-            <a href="#" class="nav-link" id='link_master_data'>
-              <i class="fas fa-th nav-icon"></i>
-              <p>
-                Master Data
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <?php
-              if ($_SESSION['jabatan'] == "ADMINKEU") {
-              ?>
-                <li class="nav-item">
-                  <a href="?page=armadaread" class="nav-link" id='armada'><i class="far fa-circle nav-icon"></i>
-                    <p>Armada</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="?page=karyawanread" class="nav-link" id='karyawan'><i class="far fa-circle nav-icon"></i>
-                    <p>Karyawan</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="?page=distributorread" class="nav-link" id="distributor"><i class="far fa-circle nav-icon"></i>
-                    <p>Distributor</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="?page=distribusiread" class="nav-link" id="distribusi"><i class="far fa-circle nav-icon"></i>
-                    <p>Distribusi</p>
-                  </a>
-                </li>
-              <?php } else if ($_SESSION['jabatan'] != "ADMINKEU") {
-              ?>
-                <li class="nav-item">
-                  <a href="?page=distribusiread" class="nav-link" id="distribusi"><i class="far fa-circle nav-icon"></i>
-                    <p>Distribusi</p>
-                  </a>
-                </li>
+            <?php } else if ($_SESSION['jabatan'] != "ADMINKEU") {
+            ?>
+              <li class="nav-item">
+                <a href="?page=distribusiread" class="nav-link" id="distribusi"><i class="far fa-circle nav-icon"></i>
+                  <p>Distribusi</p>
+                </a>
+              </li>
             <?php }; ?>
             <!-- <li class="nav-item">
               <a href="?page=armadaread" class="nav-link" id='armada'><i class="far fa-circle nav-icon"></i>
@@ -215,8 +205,8 @@
                 <p>Distribusi</p>
               </a>
             </li> -->
-            </ul>
-          </li>
+          </ul>
+        </li>
       </ul>
     </nav>
     <!-- /.sidebar-menu -->
