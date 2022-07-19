9adb31cf64c58db24a35b30ef854738420f40178
mikahikaai <mikahikaai@gmail.com>
2022-07-19 11:06:58 +0800

    update footer style

--- pages/report/reportfooter.php
+++ pages/report/reportfooter.php
@@ -1,17 +1,17 @@
 <!-- footer -->
-<hr style="color: black;">
-<table style="width: 100%; border-collapse: collapse; position: fixed;">
+<hr style="color: blue;">
+<table style="width: 100%; border-collapse: collapse;">
   <tr>
     <td align="right" width="17%" rowspan="4" style="padding-right: 10px;"><img src="../../images/logooo cropped resized compressed.png" height="50px" alt=""></td>
     <td></td>
   </tr>
   <tr>
     <!-- <td></td> -->
-    <td style="font-size: 10px; vertical-align: bottom; color: black;">Jalan Taruna Bhakti RT. 012 RW. 004 Kel. Palam, Kec. Cempaka, Kota Banjarbaru, Kalimantan Selatan - 70731</td>
+    <td style="font-size: 10px; vertical-align: bottom; color: blue;">Jalan Taruna Bhakti RT. 012 RW. 004 Kel. Palam, Kec. Cempaka, Kota Banjarbaru, Kalimantan Selatan - 70731</td>
   </tr>
   <tr>
     <!-- <td></td> -->
-    <td style="font-size: 10px; vertical-align: top;">Telp: (0511)6176347, Fax: (0511)4781368, email: amdk_amanah@amanaggroup.co.id</td>
+    <td style="font-size: 10px; vertical-align: top; color: blue;">Telp: (0511)6176347, Fax: (0511)4781368, email: amdk_amanah@amanaggroup.co.id</td>
   </tr>
   <tr>
     <!-- <td></td> -->
