584166eb555bf17a68beb8fa299ec7bdfb076509
mikahikaai <mikahikaai@gmail.com>
2022-07-23 17:05:17 +0800

    .

--- verify.php
+++ verify.php
@@ -161,14 +161,7 @@ if (isset($_GET['code'])) {
         </div>
       </div>
     </div>
-    <?php
-    if (isset($_SESSION['jabatan'])) {
-      if ($_SESSION['jabatan'] == "ADMINKEU") { ?>
-        <a href="./adminkeu/report/reportpengajuanupahdetail.php?code=<?= $row['acc_code']; ?>" class="btn btn-sm btn-warning float-right"><i class="fa fa-print"></i> Cetak</a>
-    <?php
-      }
-    }
-    ?>
+    <a href="./adminkeu/report/reportpengajuanupahdetail.php?code=<?= $row['acc_code']; ?>" class="btn btn-sm btn-warning float-right"><i class="fa fa-print"></i> Cetak</a>
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
