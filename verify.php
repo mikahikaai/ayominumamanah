92cda8f76f107b377c6476e0758789590d5ad6b7
mikahikaai <mikahikaai@gmail.com>
2022-07-23 17:07:52 +0800

    ..

--- verify.php
+++ verify.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 include 'database/database.php';
 
 $database = new Database;
@@ -161,7 +163,14 @@ if (isset($_GET['code'])) {
         </div>
       </div>
     </div>
-    <a href="./adminkeu/report/reportpengajuanupahdetail.php?code=<?= $row['acc_code']; ?>" class="btn btn-sm btn-warning float-right"><i class="fa fa-print"></i> Cetak</a>
+    <?php
+    if (isset($_SESSION['jabatan'])) {
+      if ($_SESSION['jabatan'] == "ADMINKEU") { ?>
+        <a href="./adminkeu/report/reportpengajuanupahdetail.php?code=<?= $row['acc_code']; ?>" class="btn btn-sm btn-warning float-right"><i class="fa fa-print"></i> Cetak</a>
+    <?php
+      }
+    }
+    ?>
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
