4fcd8d98f6f292656290e00b743382e3f161436c
mikahikaai <mikahikaai@gmail.com>
2022-07-24 10:48:42 +0800

    restyling

--- adminkeu/pages/cetakupah.php
+++ adminkeu/pages/cetakupah.php
@@ -9,21 +9,14 @@ include_once "../partials/cssdatatables.php";
     </div>
     <form action="report/reportpengajuanupahdetail.php" method="get" target="_blank">
       <div class="card-body">
-        <div class="row mb-2 mt-2 align-items-center">
-          <div class="col-md-2">
-            <label for="kode_verif">Kode Verifikasi</label>
+        <div class="form-group row">
+          <label for="kode" class="col-sm-2 col-form-label">Kode Verifikasi</label>
+          <div class="col-sm-4">
+            <input type="text" class="form-control" name="kode_verif" placeholder="Kode Verifikasi">
           </div>
-          <div class="col-md-1 d-flex justify-content-end">
-            <label for="kode_verif">:</label>
-          </div>
-          <div class="col-md-4">
-            <input type="text" name="acc_code" id="">
-          </div>
-          <div class="col-md-2">
-            <button type="submit" class="btn btn-warning btn-sm">
-              <i class="fa fa-print"></i> Cetak
-            </button>
-          </div>
+          <button type="submit" class="btn btn-warning btn-sm col-sm-2">
+            <i class="fa fa-print"></i> Cetak
+          </button>
         </div>
       </div>
     </form>
